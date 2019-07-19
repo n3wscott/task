25b2570ef2d7fadf84a1b4436b11272847f1b635
Scott Nichols <nicholss@google.com>
2019-07-19 16:16:30 -0700

    adding invoker to help ingress cloudevents.

---
+++ cmd/ceinvoker/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	cloudevents "github.com/cloudevents/sdk-go"
+	"github.com/kelseyhightower/envconfig"
+	"log"
+)
+
+func main() {
+	client, err := cloudevents.NewDefaultClient()
+	if err != nil {
+		panic(err)
+	}
+
+	r := Receiver{client: client}
+	if err := envconfig.Process("", &r); err != nil {
+		panic(err)
+	}
+
+	if err := client.StartReceiver(context.Background(), r.Receive); err != nil {
+		log.Fatal(err)
+	}
+}
+
+type Receiver struct {
+	client cloudevents.Client
+	Target string `envconfig:"TARGET" required:"true"`
+}
+
+func (r *Receiver) Receive(event cloudevents.Event) {
+	ctx := cloudevents.ContextWithTarget(context.Background(), r.Target)
+	if _, err := r.client.Send(ctx, event); err != nil {
+		fmt.Println(err)
+	}
+}
